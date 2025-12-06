944b7f6b7a515cb8ed77e152649bd078c80c14ef
Dushan Aluvihare <aluviharedushan@gmail.com>
2025-12-06 10:26:15 +0530

    second commit

---
+++ shopping_cart_example_2.py
@@ -0,0 +1,10 @@
+shopping_cart = []
+
+shopping_cart.append("apple")
+shopping_cart.append("banana")
+shopping_cart.append("milk")
+
+print("Shopping Cart:")
+# Print the contents of the shopping cart
+for item in shopping_cart:
+    print(item)
